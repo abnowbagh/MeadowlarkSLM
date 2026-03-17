88cc8485e9982a41525b81299d9141cc502d002c
abnowbagh <abhimanyu.nowbagh@hhu.de>
2026-03-17 16:52:45 +0100

    Add files via upload

--- Astigmatism.m
+++ Astigmatism.m
@@ -5,26 +5,34 @@ clc
 if ~libisloaded('ImageGen')
     loadlibrary('ImageGen.dll', 'ImageGen.h');
 end
-loadlibrary('Blink_C_Wrapper.dll', 'Blink_C_Wrapper.h');
+
 % read the SLM height and width. 
 width = calllib('Blink_C_wrapper', 'Get_Width');
 height = calllib('Blink_C_wrapper', 'Get_Height');
 depth = calllib('Blink_C_wrapper', 'Get_Depth');
-
+pixel=8e-6;
+Ncycles=20;
 %
 %% Astigmatism
+for i=1
 
-Ax=0.5;
-Ay=0.1;
 
 [x, y] = meshgrid(linspace(-0.5, 0.5, width), linspace(-0.5, 0.5, height));
+alpha=i*pi
 
-phi_ast= Ax*x.^2+ Ay*y.^2;
+k = Ncycles/(width*pixel);
+% %l = Ncycles/(Ny.*pixel);% cycles per metre
+phiTiltX = 2*pi*k .* X;           % linear phase in x
+phiTiltY = 0; %2*pi*l .* Y;
+ phiTilt=phiTiltX;
+%phi_ast= alpha*(x.^2-y.^2);
 
 
+isEightBit = true;
+RGB = true;
 
 % Normalize phase to [0,1] for LUT indexing
-normalized_phase = mod(phi_ast, 2*pi);
+normalized_phase = mod(phi_tilt+pi, 2*pi)/(2*pi);
 
 % Convert to 8-bit (0–255), assuming LUT expects 8-bit index
 slm_phase = uint8(normalized_phase * 255);
@@ -34,9 +42,11 @@ calllib('Blink_C_wrapper', 'Write_image', slm_phase, isEightBit);% this function
 
 imagesc(normalized_phase)
 axis image
-colormap gray
+colormap hsv
+caxis([0 1]) 
 colorbar
 title('Astigmatic Phase Mask')
-
+%pause(1.0)
+end
 
 
